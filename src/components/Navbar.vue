3006acdcef5c83f8d60dc6c71c50dd2cb49541b1
katherine Gómez <kaligocas1990@gmail.com>
2024-02-07 15:06:45 +0100

    solution menu burguer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+  
 </script>
 
 <template>
@@ -9,7 +9,7 @@
       <button class="navbar-toggler text-bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse float-sm-end" id="navbarSupportedContent" data-bs-theme="dark">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent" data-bs-theme="dark">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <RouterLink class="nav-link" to="/">Home</RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from './router'
 import axios, {isCancel, AxiosError} from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -13,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
 
 app.mount('#app')
