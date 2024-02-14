6828ba89805d0a7346bf8dd20e5b59ec38b9e4ea
katherine Gómez <kaligocas1990@gmail.com>
2024-02-14 12:32:17 +0100

    correction navabar integration mdbootstrap

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,16 @@ import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'mdb-vue-ui-kit/css/mdb.min.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 
 
 
 
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
