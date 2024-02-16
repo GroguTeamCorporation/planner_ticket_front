14e69c8e209ac119fc006360d8c9644b1aea6cd7
Natalia GF <natimetal83@gmail.com>
2024-02-16 15:31:13 +0100

    test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,61 @@
-import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils';
+import LoginComponent from '@/views/LoginView.vue';
+import axios from 'axios';
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+jest.mock('axios', () => ({
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+}));
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe('LoginComponent', () => {
+  it('renders the login form', () => {
+    const wrapper = mount(LoginComponent);
+    expect(wrapper.find('form').exists()).toBe(true);
+    expect(wrapper.find('.logo').exists()).toBe(true);
+  });
+
+  it('calls registerUser method on form submission and handles successful registration', async () => {
+    const wrapper = mount(LoginComponent);
+
+    // Mock user input
+    await wrapper.setData({ nombre: 'pepe', email: 'pepe@ejemplo.com', password: 'pepe' });
+
+    // Simulate form submission
+    await wrapper.find('.login-form').trigger('submit.prevent');
+
+    // Verify that axios.post is called with the correct data
+    expect(axios.post).toHaveBeenCalledWith('http://localhost:8080/api/v1/users', {
+      nombre: 'pepe',
+      email: 'pepe@ejemplo.com',
+      password: 'pepe',
+    });
+
+    // Wait for the next tick to allow the asynchronous registerUser method to complete
+    await wrapper.vm.$nextTick();
+
+    // Ensure that isSubmitting is set to false after the registration
+    expect(wrapper.vm.isSubmitting).toBe(false);
+
+   
+  });
+
+  it('handles error during registration', async () => {
+    // Set up the mock to simulate an error during registration
+    axios.post.mockRejectedValueOnce(new Error('Registration failed'));
+
+    const wrapper = mount(LoginComponent);
+
+    // Mock user input
+    await wrapper.setData({ nombre: 'pepe', email: 'pepe@example.com', password: 'p' });
+
+    // Simulate form submission
+    await wrapper.find('.login-form').trigger('submit.prevent');
+
+    // Wait for the next tick to allow the asynchronous registerUser method to complete
+    await wrapper.vm.$nextTick();
+
+    // Ensure that isSubmitting is set to false after the failed registration
+    expect(wrapper.vm.isSubmitting).toBe(false);
+
+   
+  });
+});
